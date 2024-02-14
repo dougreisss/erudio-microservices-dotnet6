e4a56287605db72d2cd4f96a8313f63c65a58b1f
dougreisss <douglas_rrs@hotmail.com>
2024-02-13 21:39:45 -0300

    Configurando a estrutura do primeiro microserviço

---
+++ S07_ErudioMicroServices.NET6-FirstMicroservices/GeekShooping/GeekShopping.ProductAPI/Model/Product.cs
@@ -0,0 +1,9 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+
+namespace GeekShopping.ProductAPI.Model
+{
+    public class Product : BaseEntity
+    {
+        public int MyProperty { get; set; }
+    }
+}
