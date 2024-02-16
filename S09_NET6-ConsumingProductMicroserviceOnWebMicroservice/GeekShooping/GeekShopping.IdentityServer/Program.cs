c963abc39bb6128d02cd8360c0fe5d16bbee38d8
dougreisss <douglas_rrs@hotmail.com>
2024-02-16 11:31:26 -0300

    Add identity configuration

--- S09_NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShooping/GeekShopping.IdentityServer/Program.cs
+++ S09_NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShooping/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,38 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddDbContext<MySqlContext>(options => options.
+        UseMySql(configuration.GetConnectionString("MySqlConnection"),
+            new MySqlServerVersion(
+                new Version())));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySqlContext>()
+    .AddDefaultTokenProviders();
+
+var builderServices = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+.AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builderServices.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -10,9 +41,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
