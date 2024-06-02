6f891a34fa1f10ca771b22a2bc421ea39c1bf107
dougreisss <douglas_rrs@hotmail.com>
2024-06-02 15:50:04 -0300

    add CartApi

---
+++ S09_NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShooping/GeekShopping.CartApi/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using GeekShopping.CartApi.Data.ValueObjects;
+using GeekShopping.CartApi.Repository.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.CartApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult<CartVO>> FindById(string userId)
+        {
+            var cart = await _cartRepository.FindCartByUserId(userId);
+
+            if (cart is null) { return NotFound(); }
+
+            return Ok(cart);
+        }
+
+        [HttpPost("add-cart")]
+        public async Task<ActionResult<CartVO>> AddCart(CartVO cartVO)
+        {
+            var cart = await _cartRepository.SaveOrUpdateCart(cartVO);
+
+            if (cart is null) { return NotFound(); }
+
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart")]
+        public async Task<ActionResult<CartVO>> UpdateCart(CartVO cartVO)
+        {
+            var cart = await _cartRepository.SaveOrUpdateCart(cartVO);
+
+            if (cart is null) { return NotFound(); }
+
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<CartVO>> RemoveCart(int id)
+        {
+            var status = await _cartRepository.RemoveFromCart(id);
+
+            if (status) { return BadRequest(); }
+
+            return Ok(status);
+        }
+    }
+}
---
+++ S09_NET6-ConsumingProductMicroserviceOnWebMicroservice/GeekShooping/GeekShopping.CartApi/Model/CartDetail.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.CartApi.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartApi.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        public long CartHeaderId { get; set; }
+
+        [ForeignKey(nameof(CartHeaderId))]
+        public virtual CartHeader CartHeader { get; set; }
+        public long ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+
+        [Column("count")]
+        public int Count { get; set; }
+    }
+}
